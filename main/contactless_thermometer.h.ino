1317d148bced45523a3c5b758c31c66497ce7fdb
Cameron Ellis <71197594+cameron-ellis@users.noreply.github.com>
2021-02-11 16:16:40 -0600

    add header file
    added the header file to the main folder

---
+++ main/contactless_thermometer.h.ino
@@ -0,0 +1,12 @@
+#ifndef CONTACTLESS_THERMOMETER_H
+#define CONTACTLESS_THERMOMETER_H
+
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <SparkFunMLX90614.h> // SparkFunMLX90614 Arduino library
+
+
+#endif
